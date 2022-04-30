a146fca35bcb20f2e0660d4f2de05aaf028d06a0
CyprienCambus <cyprien.cambus@gmail.com>
2022-04-30 15:15:57 +0200

    inital commit

---
+++ example.R
@@ -0,0 +1,143 @@
+library(shiny)
+library(ggplot2)
+library(plotly)
+library(dplyr)
+library(promises)
+library(future)
+
+plan(multisession)
+
+
+
+ui <- fluidPage(
+  selectInput("id1", "Choose:", choices = unique(as.character(iris$Species))),
+  dataTableOutput("my_data"),
+  plotlyOutput("my_plot")
+)
+
+
+server <- function(input, output, session) {
+  
+  iris_tbl <- reactive({
+    iris %>% filter(Species == input$id1)
+  })
+  
+  
+  
+  output$my_data <- renderDataTable({
+    iris_tbl()
+  })
+  
+  
+  
+  data_to_plot <- reactiveVal()
+  observe({
+    # see: https://cran.r-project.org/web/packages/promises/vignettes/shiny.html
+    # Shiny-specific caveats and limitations
+    df <- iris_tbl()
+    # see https://github.com/rstudio/promises/issues/23#issuecomment-386687705
+    
+    future_promise({
+      Sys.sleep(10)
+      ggplot(df, aes(x=Sepal.Length)) + geom_histogram()
+    }) %...>%
+      data_to_plot() %...!%  # Assign to data
+      (function(e) {
+        data(NULL)
+        warning(e)
+        session$close()
+      }) # error handling
+    
+    
+    
+    # Hide the async operation from Shiny by not having the promise be
+    # the last expression.
+    NULL
+    
+  })
+  
+  library(shiny)
+library(ggplot2)
+library(plotly)
+library(dplyr)
+library(promises)
+library(future)
+
+plan(multisession)
+
+
+
+ui <- fluidPage(
+  selectInput("id1", "Choose:", choices = unique(as.character(iris$Species))),
+  dataTableOutput("my_data"),
+  plotlyOutput("my_plot")
+)
+
+
+server <- function(input, output, session) {
+  
+  iris_tbl <- reactive({
+    iris %>% filter(Species == input$id1)
+  })
+  
+  
+  
+  output$my_data <- renderDataTable({
+    iris_tbl()
+  })
+  
+  
+  
+  data_to_plot <- reactiveVal()
+  observe({
+    # see: https://cran.r-project.org/web/packages/promises/vignettes/shiny.html
+    # Shiny-specific caveats and limitations
+    dataframe <- iris_tbl()
+    # see https://github.com/rstudio/promises/issues/23#issuecomment-386687705
+    
+    future_promise({
+      Sys.sleep(10)
+      ggplot(data2, aes(x=Sepal.Length)) + geom_histogram()
+    }) %...>%
+      data_to_plot() %...!%  # Assign to data
+      (function(e) {
+        data(NULL)
+        warning(e)
+        session$close()
+      }) # error handling
+    
+    
+    
+    # Hide the async operation from Shiny by not having the promise be
+    # the last expression.
+    NULL
+    
+  })
+  
+  
+  
+  output$my_plot <- renderPlotly({
+    req(data_to_plot())
+    ggplotly(data_to_plot())
+  })
+  
+  
+  
+}
+
+
+
+shinyApp(ui, server)
+  
+  output$my_plot <- renderPlotly({
+    req(data_to_plot())
+    ggplotly(data_to_plot())
+  })
+  
+  
+  
+}
+
+
+
+shinyApp(ui, server)
